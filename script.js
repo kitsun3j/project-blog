824522ed8ffd2af2108c3fba7c7392ca4ca5fc0a
kitsun3j <107376593+kitsun3j@users.noreply.github.com>
2022-06-13 16:22:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function abrirAlerta(){
+    swal({
+        title: "Olá!",
+        text: "Obs: o site ta em construção, talvez algumas coisas funções ou páginas não funcionem, não fiz esse site com intuito profissional, fiz como uma forma de treinar meus conhecimentos da area de developer.",
+        icon: "warning",
+        button: "Aww yiss!",
+      });
+}
